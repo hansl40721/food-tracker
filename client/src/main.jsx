a3370b51c730baae9dbc4e1336f224a9fd30cac6
hansl40721 <lucas.hansenst@gmail.com>
2025-01-14 16:07:58 -0600

    first try at UI, needs debugging

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from  'react-router-dom';
+
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Grocery from './pages/Grocery.jsx'
+import Lists from './pages/Lists.jsx'
+import Pantry from './pages/Pantry.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong Page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        path: '/pantry',
+        element: <Pantry />
+      }, {
+        path: '/lists',
+        element: <Lists />
+      }, {
+        path: '/grocery',
+        element: <Grocery />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 )
